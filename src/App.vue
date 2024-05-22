bff3330c9f2147a10c5cb6b06a50ff73723848e9
Naibuu <hawkwer141099@gmail.com>
2024-05-22 19:16:38 +0300

    feat: tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
-<template>
-</template>
+<template><h1 class="text-3xl font-bold underline">Hello world!</h1></template>
 
-<style scoped>
-</style>
+<style scoped></style>
