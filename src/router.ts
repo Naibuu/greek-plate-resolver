455cd04bfe4fa6c8ca0f0b39417c681627433a6c
Naibuu <hawkwer141099@gmail.com>
2024-05-23 14:22:29 +0300

    chore: remove category for now

--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,10 @@
 import { createWebHistory, createRouter } from 'vue-router'
 
 import LocationView from './views/Location.vue'
-import CategoryView from './views/Category.vue'
 import NotFoundView from './views/NotFound.vue'
 
 const routes = [
     { path: '/', component: LocationView },
-    { path: '/category', component: CategoryView },
     { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFoundView },
 ]
 
